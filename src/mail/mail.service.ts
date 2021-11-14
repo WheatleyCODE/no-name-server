faa8a8e3b0d21bb2863b1fb5f9cf027204d66286
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-14 10:52:31 +0300

    Add mail service

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+@Injectable()
+export class MailService {
+  private transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'qb.wheatley@gmail.com',
+      pass: '89108821300',
+    },
+  } as SMTPTransport.Options);
+
+  async sendActivationMail(to: string, link: string) {
+    try {
+      console.log('Отправляем письмо: ' + to);
+
+      await this.transporter.sendMail({
+        from: process.env.SMTP_USER,
+        to,
+        subject: 'Активация аккаунта' + process.env.API_SERVER_IP,
+        text: '',
+        html: `
+          <div>
+            <h1>Для активации аккаунта перейдите по ссылке</h1>
+            <a href="${link}">Ссылка для активации аккаунта</a>
+          </div>
+        `,
+      });
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        { message: 'Ошибка при отправке письма' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
