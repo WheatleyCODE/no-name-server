931bfa789802723cff1fcf66a6dd15fa006f0a30
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-14 10:56:56 +0300

    Add auth, roles guards, add auth decorator

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { TokensService } from '../tokens/tokens.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private tokensService: TokensService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new UnauthorizedException({
+        message: 'User is not authenticated',
+      });
+    }
+    const bearer = authHeader.split(' ')[0];
+    const token = authHeader.split(' ')[1];
+
+    if (bearer !== 'Bearer' || !token) {
+      throw new UnauthorizedException({
+        message: 'User is not authenticated',
+      });
+    }
+
+    const user = this.tokensService.verifyAccessToken(token);
+
+    if (!user.isActivated) {
+      throw new UnauthorizedException({
+        message: 'Аккаунт не активирован',
+      });
+    }
+    req.user = user;
+    return true;
+  }
+}
---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,59 @@
+import { TokensService } from '../tokens/tokens.service';
+import { ROLES_KEY } from './roles.auth.decorator';
+
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private tokensService: TokensService,
+    private reflector: Reflector,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const requiredRoles = this.reflector.getAllAndOverride(ROLES_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+
+      if (!requiredRoles) {
+        return true;
+      }
+      const req = context.switchToHttp().getRequest();
+      const authHeader = req.headers.authorization;
+      const bearer = authHeader.split(' ')[0];
+      const token = authHeader.split(' ')[1];
+
+      if (bearer !== 'Bearer' || !token) {
+        throw new UnauthorizedException({
+          message: 'User is not authenticated',
+        });
+      }
+
+      const user = this.tokensService.verifyAccessToken(token);
+
+      if (!user.isActivated) {
+        throw new UnauthorizedException({
+          message: 'Аккаунт не активирован',
+        });
+      }
+
+      req.user = user;
+      return user.role === requiredRoles;
+    } catch (e) {
+      throw new HttpException('Нет прав или авторизации', HttpStatus.FORBIDDEN);
+    }
+  }
+}
